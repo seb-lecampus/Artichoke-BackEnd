3ebdb506b9cafd916023c42bc0c1a392b35147b0
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-04-16 15:57:22 +0200

    language : create & insert

---
+++ sql_scripts/create/language.sql
@@ -0,0 +1,6 @@
+CREATE TABLE language (
+    reference_language INT AUTO_INCREMENT PRIMARY KEY,
+    translate_english_language VARCHAR(255),
+    translate_french_language VARCHAR(255),
+    translate_italian_language VARCHAR(255)
+)
