9821ad89380137b2c8769b8d37fe6ca3cbd408b1
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-04-16 17:03:05 +0200

    create script : user

---
+++ sql_scripts/create/user.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    firstName_user TINYTEXT,
+    lastName_user TINYTEXT,
+    date_of_birth_user DATE,
+    email_user TINYTEXT,
+    telephone_user TINYTEXT,
+    gender_user TINYTEXT,
+    id_payment INT,
+    id_account INT,
+    FOREIGN KEY (id_payment) REFERENCES payment(id_payment),
+    FOREIGN KEY (id_account) REFERENCES account(id_account)
+)
