0abc722d5065c5cbeb2fb5350337c3b56c5447d2
Ibrahima COULIBALY <ibrahima.coulibaly@Digital-Grenoble.local>
2024-04-16 16:59:25 +0200

    insert in to tables event, company, account

---
+++ sql_scripts/insert/account.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (login_account, password_account, authorities)
+VALUES
+    ('test@gmail.com', 'password', 'access')
---
+++ sql_scripts/insert/event.sql
@@ -0,0 +1,3 @@
+INSERT INTO event (title_event, seat_event, date_start_event, date_end_event, image_event, description_event)
+VALUES
+    ('tech&fest', '33 rue grand place', '2024-04-18', '2024-04-19','https://www.mountain-planet.com/wp-content/themes/mountain-planet/www/img/foule.webp', 'forum' )
