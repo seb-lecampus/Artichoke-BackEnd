953ff664d8e6e396a7cd45a53bf93c7292688117
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-04-16 15:48:23 +0200

    advantage : create & insert

---
+++ sql_scripts/create/advantage.sql
@@ -0,0 +1,5 @@
+CREATE TABLE advantage (
+    reference_advantage INT AUTO_INCREMENT PRIMARY KEY,
+    title_advantage VARCHAR(50),
+    description_advantage VARCHAR(255)
+)
---
+++ sql_scripts/insert/advantage.sql
@@ -0,0 +1,3 @@
+INSERT INTO advantage (title_advantage, description_advantage)
+VALUES
+    ('Happiness', 'This hotel is the best')
