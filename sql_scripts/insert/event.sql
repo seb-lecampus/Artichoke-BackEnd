3eb20f26646aa415df6d5a4fc494e95547e6fef9
serge.szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-04-19 11:35:15 +0200

    Up-To-Date insert_db.sql & install_db.sql

--- sql_scripts/insert/event.sql
+++ sql_scripts/insert/event.sql
@@ -2,5 +2,4 @@ INSERT INTO event (title_event, seat_event, date_start_event, date_end_event, im
 VALUES
     ('tech&fest', '1000', '2024-04-18', '2024-04-19','https://www.mountain-planet.com/wp-content/themes/mountain-planet/www/img/foule.webp', 'forum' ),
     ('bad boy event', '12', '2024-06-06', '2024-06-08', '1', '1'),
-    ('mountain-planet', '2000', '2024-04-16', '2024-04-18', 'https://www.mountain-planet.com/', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
-);
+    ('mountain-planet', '2000', '2024-04-16', '2024-04-18', 'https://www.mountain-planet.com/', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt');
--- sql_scripts/install_db.sql
+++ sql_scripts/install_db.sql
@@ -5,48 +5,49 @@ USE artichoke;
 -- Creation Dependency 0 | 10 tables
 
 CREATE TABLE account(
-    id_account BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    login_account TINYTEXT,
-    password_account TINYTEXT,
-    authorities_account TINYTEXT
+    id_account BIGINT AUTO_INCREMENT PRIMARY KEY,
+    login_account TINYTEXT NOT NULL,
+    password_account TINYTEXT NOT NULL,
+    authorities_account TINYTEXT NOT NULL
 );
 
 CREATE TABLE advantage (
-    reference_advantage BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title_advantage TINYTEXT,
-    description_advantage TINYTEXT
+   reference_advantage BIGINT AUTO_INCREMENT PRIMARY KEY,
+   title_advantage TINYTEXT NOT NULL,
+   description_advantage TINYTEXT NOT NULL
 );
 
 CREATE TABLE company(
-    id_company BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name_company TINYTEXT,
-    address_company TINYTEXT,
-    email_company TINYTEXT,
+    id_company BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name_company TINYTEXT NOT NULL,
+    address_company TINYTEXT NOT NULL,
+    email_company TINYTEXT NOT NULL,
     telephone_company TINYTEXT
 );
 
 CREATE TABLE event(
-    id_event BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    title_event TINYTEXT,
-    seat_event TINYTEXT,
-    date_start_event DATE,
-    date_end_event DATE,
+    id_event BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title_event TINYTEXT NOT NULL,
+    seat_event TINYTEXT NOT NULL,
+    date_start_event DATE NOT NULL,
+    date_end_event DATE NOT NULL,
     image_event TINYTEXT,
     description_event TINYTEXT
 );
 
+
 CREATE TABLE extra(
-    id_extra BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name_extra TINYTEXT,
-    price_type_extra DECIMAL(10, 2),
+    id_extra BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name_extra TINYTEXT NOT NULL,
+    price_type_extra DECIMAL(10, 2) NOT NULL,
     description_extra TINYTEXT
 );
 
 CREATE TABLE language (
     reference_language BIGINT AUTO_INCREMENT PRIMARY KEY,
-    translate_english_language TINYTEXT,
-    translate_french_language TINYTEXT,
-    translate_italian_language TINYTEXT
+    translate_english_language TINYTEXT NOT NULL,
+    translate_french_language TINYTEXT NOT NULL,
+    translate_italian_language TINYTEXT NOT NULL
 );
 
 CREATE TABLE payment (
@@ -58,22 +59,22 @@ CREATE TABLE payment (
 );
 
 CREATE TABLE promo(
-    reference_promo BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    reference_promo BIGINT AUTO_INCREMENT PRIMARY KEY,
     price_promo DECIMAL(10, 2),
-    date_start_promo DATE,
-    date_end_promo DATE,
+    date_start_promo DATE NOT NULL,
+    date_end_promo DATE NOT NULL,
     image_promo TINYTEXT
 );
 
 CREATE TABLE type_room(
-    ref_type BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    ref_type BIGINT AUTO_INCREMENT PRIMARY KEY,
     name TINYTEXT,
     price DECIMAL(10, 2),
     size_room TINYTEXT
 );
 
 CREATE TABLE video(
-    reference_video BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    reference_video BIGINT AUTO_INCREMENT PRIMARY KEY,
     titre_video TINYTEXT,
     description_video TINYTEXT,
     content_video TINYTEXT
@@ -83,10 +84,10 @@ CREATE TABLE video(
 
 CREATE TABLE user (
     id_user BIGINT AUTO_INCREMENT PRIMARY KEY,
-    firstName_user TINYTEXT,
-    lastName_user TINYTEXT,
-    date_of_birth_user DATE,
-    email_user TINYTEXT,
+    firstName_user TINYTEXT NOT NULL,
+    lastName_user TINYTEXT NOT NULL,
+    date_of_birth_user DATE NOT NULL,
+    email_user TINYTEXT NOT NULL,
     telephone_user TINYTEXT,
     gender_user TINYTEXT,
     id_payment BIGINT,
@@ -96,30 +97,30 @@ CREATE TABLE user (
 );
 
 CREATE TABLE newscast (
-  reference_newscast BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  id_event BIGINT,
-  title_newscast TINYTEXT,
-  image_newscast TINYTEXT,
-  description_newscast TINYTEXT,
-  FOREIGN KEY (id_event) REFERENCES event(id_event)
+    reference_newscast BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title_newscast TINYTEXT NOT NULL,
+    image_newscast TINYTEXT,
+    description_newscast TINYTEXT NOT NULL,
+    id_event BIGINT,
+    FOREIGN KEY (id_event) REFERENCES event(id_event)
 );
 
 CREATE TABLE room (
-  number_room BIGINT PRIMARY KEY NOT NULL,
-  ref_type BIGINT,
-  FOREIGN KEY (ref_type) REFERENCES type_room(ref_type)
+    number_room BIGINT AUTO_INCREMENT PRIMARY KEY,
+    ref_type BIGINT,
+    FOREIGN KEY (ref_type) REFERENCES type_room(ref_type)
 );
 
 -- Creation Dependency 2 | 4 Tables
 
 CREATE TABLE reservation (
     id_reservation BIGINT AUTO_INCREMENT PRIMARY KEY,
+    start_date_reservation DATE NOT NULL,
+    end_date_reservation DATE NOT NULL,
+    nbr_person_reservation INT NOT NULL,
     id_user BIGINT,
     number_room BIGINT,
     reference_promo BIGINT,
-    start_date_reservation DATE,
-    end_date_reservation DATE,
-    nbr_person_reservation INT,
     FOREIGN KEY (id_user) REFERENCES user(id_user),
     FOREIGN KEY (number_room) REFERENCES room(number_room),
     FOREIGN KEY (reference_promo) REFERENCES promo(reference_promo)
@@ -127,21 +128,21 @@ CREATE TABLE reservation (
 
 CREATE TABLE notice (
     id_notice BIGINT AUTO_INCREMENT PRIMARY KEY,
-    reference_notice INT,
-    title_notice TINYTEXT,
+    rate_notice numeric CHECK (rate_notice >= 1 and rate_notice <= 5),
+    title_notice TINYTEXT NOT NULL,
     image_notice TEXT,
-    note_notice TEXT,
-    lastName_notice TINYTEXT,
+    description_notice TEXT,
+    lastName_notice TINYTEXT COMMENT "user can write any name, like different name of their",
     id_user BIGINT,
     FOREIGN KEY (id_user) REFERENCES user(id_user)
 );
 
 CREATE TABLE user_event(
-   id_user_event BIGINT AUTO_INCREMENT PRIMARY KEY,
-   id_user BIGINT,
-   id_event BIGINT,
-   FOREIGN KEY (id_user) REFERENCES user(id_user),
-   FOREIGN KEY (id_event) REFERENCES event(id_event)
+    id_user_event BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_user BIGINT,
+    id_event BIGINT,
+    FOREIGN KEY (id_user) REFERENCES user(id_user),
+    FOREIGN KEY (id_event) REFERENCES event(id_event)
 );
 
 CREATE TABLE user_company(
@@ -164,12 +165,12 @@ CREATE TABLE reservation_extra(
 
 CREATE TABLE facture (
     reference_facture BIGINT AUTO_INCREMENT PRIMARY KEY,
+    date_facture DATE NOT NULL,
+    summary_facture TINYTEXT NOT NULL,
+    state_facture ENUM('Paid', 'Unpaid', 'Canceled', 'Refused') DEFAULT 'Unpaid',
     id_reservation BIGINT,
     id_user BIGINT,
     id_payment BIGINT,
-    date_facture DATE,
-    summary_facture TINYTEXT,
-    state_facture TINYTEXT,
     FOREIGN KEY (id_reservation) REFERENCES reservation(id_reservation),
     FOREIGN KEY (id_user) REFERENCES user(id_user),
     FOREIGN KEY (id_payment) REFERENCES payment(id_payment)
